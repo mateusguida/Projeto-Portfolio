59d65f6f11ba11b28a634ac5fb8983969b0b7bee
mateusguida <guidamaster@gmail.com>
2025-10-27 16:22:39 -0300

    Responsividade para telas maiores

--- scripts/scriptportf.js
+++ scripts/scriptportf.js
@@ -30,9 +30,7 @@ navLinks.forEach(link => {
     const target = document.querySelector(this.getAttribute('href'));
     if (target) {
       const headerHeight = document.querySelector('header').offsetHeight;
-      alert(headerHeight);
       const targetPosition = target.offsetTop - headerHeight - 20;
-      alert(targetPosition);
       window.scrollTo({
         top: targetPosition,
         behavior: 'smooth'
